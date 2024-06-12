9a05e904b75717ece6b2263787d22505736cba6f
Aaren Rice <aaren.rice@gmail.com>
2024-06-11 20:49:12 -0700

    hooking tailwind in, adding modal with map, and start search bar

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,40 @@
-import { useState } from "react";
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Button } from "@mui/material";
-import GoogleMap from "./components/GoogleMap/map";
-import AddLocationModal from "./components/AddLocationModal/add-location-modal";
+import { useState } from 'react';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Button } from '@mui/material';
+import GoogleMap from './components/GoogleMap/map';
+import AddLocationModal from './components/AddLocationModal/add-location-modal';
+import './index.css';
 
 const App = () => {
-  const [open, setOpen] = useState<boolean>(false);
+	const [open, setOpen] = useState<boolean>(false);
 
-  return (
-    <>
-      <Button onClick={() => setOpen(!open)}>Test me out</Button>
-      <AddLocationModal open={open} setOpen={setOpen}>
-        <GoogleMap />
-      </AddLocationModal>
-    </>
-  );
+	return (
+		<div className="flex h-screen flex-col">
+			<div className="flex flex-auto justify-center items-center">
+				<Button
+					onClick={() => setOpen(!open)}
+					className="h-12 w-36 border-2"
+					variant="contained"
+				>
+					Test me out
+				</Button>
+				<AddLocationModal open={open} setOpen={setOpen}>
+					<GoogleMap />
+				</AddLocationModal>
+			</div>
+		</div>
+	);
 };
 
 export default App;
 
 export function renderToDom(container: HTMLElement) {
-  const root = createRoot(container);
+	const root = createRoot(container);
 
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+	root.render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+	);
 }
--- src/components/GoogleMap/map.tsx
+++ src/components/GoogleMap/map.tsx
@@ -1,51 +1,55 @@
-// import React, { useEffect, useState, useRef, useCallback } from "react";
-// import { createRoot } from "react-dom/client";
+import React, { useState } from 'react';
 
 import {
-  APIProvider,
-  Map,
-  // useMap,
-  // AdvancedMarker,
-  MapCameraChangedEvent,
-  // Pin,
-} from "@vis.gl/react-google-maps";
+	APIProvider,
+	Map,
+	MapCameraChangedEvent,
+	ControlPosition,
+} from '@vis.gl/react-google-maps';
 
-// import { MarkerClusterer } from "@googlemaps/markerclusterer";
-// import type { Marker } from "@googlemaps/markerclusterer";
-
-// import { Circle } from "../circle";
-import PoiMarkers from "./poi-marker";
-import { Poi } from "../../types";
+import PoiMarkers from './poi-marker';
+import { Poi } from '../../types';
+import { CustomMapControl } from './map-control';
 
 const locations: Poi[] = [
-  {
-    key: "American Junkie",
-    location: { lat: 33.86200633981369, lng: -118.40043937895398 },
-  },
+	{
+		key: 'American Junkie',
+		location: { lat: 33.86200633981369, lng: -118.40043937895398 },
+	},
 ];
 
-const GoogleMap = () => (
-  <APIProvider
-    apiKey={import.meta.env.VITE_GM_API_KEY}
-    onLoad={() => console.log("Maps API has loaded.")}
-  >
-    <Map
-      mapId={import.meta.env.VITE_GM_ID_KEY}
-      defaultZoom={13}
-      style={{ width: "80vw", height: "80vh" }}
-      defaultCenter={{ lat: 33.860664, lng: -118.4009608 }}
-      // onCameraChanged={(ev: MapCameraChangedEvent) =>
-      //   console.log(
-      //     "camera changed:",
-      //     ev.detail.center,
-      //     "zoom:",
-      //     ev.detail.zoom
-      //   )
-      // }
-    >
-      <PoiMarkers pois={locations} />
-    </Map>
-  </APIProvider>
-);
+const GoogleMap = () => {
+	const [selectedPlace, setSelectedPlace] =
+		useState<google.maps.places.PlaceResult | null>(null);
+	return (
+		<APIProvider
+			apiKey={import.meta.env.VITE_GM_API_KEY}
+			onLoad={() => console.log('Maps API has loaded.')}
+		>
+			<div className="flex-grow">
+				<Map
+					mapId={import.meta.env.VITE_GM_ID_KEY}
+					defaultZoom={13}
+					defaultCenter={{ lat: 33.860664, lng: -118.4009608 }}
+					className="h-[80vh] w-full"
+					// onCameraChanged={(ev: MapCameraChangedEvent) =>
+					//   console.log(
+					//     "camera changed:",
+					//     ev.detail.center,
+					//     "zoom:",
+					//     ev.detail.zoom
+					//   )
+					// }
+				>
+					<PoiMarkers pois={locations} />
+				</Map>
+				<CustomMapControl
+					controlPosition={ControlPosition.TOP_CENTER}
+					onPlaceSelect={setSelectedPlace}
+				/>
+			</div>
+		</APIProvider>
+	);
+};
 
 export default GoogleMap;
